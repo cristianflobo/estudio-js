2ed45cbd9cd18e1301fc3535186a2754aff3f0a0
cristian lobo <cristianflobo@hotmail.com>
2022-01-31 17:04:04 -0500

    OK

--- 08-React-Estado-LifeCycle/homework/src/App.js
+++ 08-React-Estado-LifeCycle/homework/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Nav from "./components/Nav"
+import Cards from "./components/Cards"
 
 export default function App() {
   return (
     <div className="App">
-      { /* Tu código acá: */ }
-      <h1>Título</h1>
+        <Nav />
+        <Cards />
     </div>
   );
 }
--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import Logo from '../logoHenry.png'
 import SearchBar from './SearchBar.jsx';
 import './Nav.css';
+import { Button } from 'reactstrap';
 
 function Nav({onSearch}) {
   return (
-    <div>Nav</div>
+    <div><nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand">Henry</a>
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </nav></div>
   );
 };
 
